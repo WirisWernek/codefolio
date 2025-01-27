0d985bdfd4eb99fc27b86c0a68a48271f365399b
Wiris Wernek <wiriswernek@gmail.com>
2025-01-26 22:15:26 -0300

    adiciona funcionalidade de fechamento ao cabeçalho do editor; atualiza estilos para garantir responsividade e espaçamento adequado

--- src/app/shared/components/editor-header/editor-header.component.ts
+++ src/app/shared/components/editor-header/editor-header.component.ts
@@ -1,13 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-editor-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './editor-header.component.html',
   styleUrl: './editor-header.component.scss'
 })
 export class EditorHeaderComponent {
+
+	router = inject(Router);
+
 	languageIcon = input.required<string>();
 	pageName = input.required<string>();
+
+	closeClick() {
+		this.router.navigateByUrl('home');
+	}
 }
